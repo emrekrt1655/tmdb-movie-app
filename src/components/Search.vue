ba5b6279c7cc904ae3ca614b5fc0da93236204c0
eku <emre.kurt@dialogdata.de>
2024-03-26 11:19:45 +0100

    add some style to the search container

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="search-movie w-full h-96 mt-6">
-        <div class="image-box w-4/5 h-96 m-auto flex flex-col justify-center items-center bg-no-repeat bg-cover" :style="{
-            backgroundImage: `url(${baseUrl}${movieStore.popularMoviesInfoList[randomNumber]?.backdrop_path})`,
-            filter: 'grayscale(100%)'
-        }">
-            <input class="search-input h-16 w-1/2 text-lg rounded-lg" type="text" />
+        <div class="image-box w-4/5 h-96 m-auto flex flex-col justify-center items-center bg-no-repeat bg-cover grayscale"
+            :style="{
+                backgroundImage: `url(${baseUrl}${movieStore.popularMoviesInfoList[randomNumber]?.backdrop_path})`
+            }">
+            <input class="search-input h-16 w-1/2 text-lg" type="text" placeholder="Search..." />
             <p class="movie-info mt-6 text-white font-bold text-lg">
-                The Movie in the Picture: {{ movieStore.popularMoviesInfoList[randomNumber]?.title }}
+                The Movie in the Picture: <strong class="font-black">{{
+                    movieStore.popularMoviesInfoList[randomNumber]?.title
+                }} </strong>
             </p>
         </div>
     </div>
@@ -20,4 +22,16 @@ console.log(baseUrl)
 const randomNumber = Math.floor(Math.random() * 20)
 </script>
 
-<style></style>
+<style>
+.search-input {
+    background-color: rgba(255, 255, 255, 0.3);
+    border: 2px solid rgba(255, 255, 255, 0.3);
+    border-radius: 30px;
+    color: white
+}
+
+.search-input::placeholder {
+    color: white;
+    opacity: 1;
+}
+</style>
